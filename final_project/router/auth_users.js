54e485ee9f9208e721f0b6105ad1572f64ffb918
I-choose-charmander <sheridantechrequest@gmail.com>
2023-09-25 00:45:48 -0400

    save point to task 10

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
+
 let users = [{
     "username":"I-choose-charmander",
      "password":"Welcome"
@@ -32,7 +33,25 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-    return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+              accessToken,username
+          }
+          return res.status(200).send("User successfully logged in");
+      } else {
+          return res.status(208).json({message: "Invalid Login. Check username and password"});
+      }
 });
 
 // Add a book review
@@ -40,13 +59,26 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     const isnb = req.params.isbn;
     const reviews = req.body.review;
     const username = req.session.authorization.username;
-    if(books[isbn]){
-        let book = books[isbn];
-        book.reviews[username] = review;
+    if(books[isnb]){
+        let book = books[isnb];
+        book.reviews[username] = reviews;
         return res.status(200).send("Review successfully posted");
     }
     else {
-        return res.status(404).json({message: `ISBN ${isbn} not found`});
+        return res.status(404).json({message: `ISBN ${isnb} not found`});
+    }
+});
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isnb = req.params.isbn;
+    const reviews = req.body.review;
+    const username = req.session.authorization.username;
+    if(books[isnb]){
+        let book = books[isnb];
+        book.reviews[username] = delete[reviews];
+        return res.status(200).send("Review successfully deleted");
+    }
+    else {
+        return res.status(404).json({message: `Review not found`});
     }
 });
 
